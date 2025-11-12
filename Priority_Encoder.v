819a5c73135f75ef5415d489e0d22c9fe0383bdd
Tanisha227 <tanishameena2115@ce.du.ac.in>
2025-11-12 16:08:22 +0530

    Update Priority_Encoder.v

--- Priority_Encoder.v
+++ Priority_Encoder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/06/2025 09:43:09 PM
-// Design Name: 
-// Module Name: Priority_Encoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Priority_Encoder(
     input a, b, c, d,
