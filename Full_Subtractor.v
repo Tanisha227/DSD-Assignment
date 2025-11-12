ad812ce7420f897ace6f03eef09ba7662da6d4d8
Tanisha227 <tanishameena2115@ce.du.ac.in>
2025-11-12 16:06:36 +0530

    Update Full_Subtractor.v

--- Full_Subtractor.v
+++ Full_Subtractor.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/12/2025 02:32:36 PM
-// Design Name: 
-// Module Name: Full_Subtractor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Full_Subtractor(
     input A,B,Bin,
