1fb81388bebbfa5e481d5af58823b4ab8b01bef2
Tanisha227 <tanishameena2115@ce.du.ac.in>
2025-11-12 16:09:26 +0530

    Update TB_Decoder.v

--- TB_Decoder.v
+++ TB_Decoder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/06/2025 10:51:47 PM
-// Design Name: 
-// Module Name: TB_Decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module TB_Decoder(
 
