af799290fa8f4bc19a7df3f1d5eff7449c9e028a
Tanisha227 <tanishameena2115@ce.du.ac.in>
2025-11-12 16:05:54 +0530

    Clean up comments in Decoder.v
    Removed header comments from Decoder.v file.

--- Decoder.v
+++ Decoder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/06/2025 09:47:39 PM
-// Design Name: 
-// Module Name: Decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Decoder(
    input a, b,
