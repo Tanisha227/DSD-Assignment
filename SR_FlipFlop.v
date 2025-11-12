c2d3f3d862caebc4fc7013d2c3e574bc247c78d4
Tanisha227 <tanishameena2115@ce.du.ac.in>
2025-11-12 16:08:39 +0530

    Update SR_FlipFlop.v

--- SR_FlipFlop.v
+++ SR_FlipFlop.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/12/2025 02:38:57 PM
-// Design Name: 
-// Module Name: SR_FlipFlop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module SR_Flipflop(
     input S,R,
