1eec693747606af7a478ee13caea7c9e237d3c64
Tanisha227 <tanishameena2115@ce.du.ac.in>
2025-11-12 16:08:52 +0530

    Clean up comments in SR_Latch.v
    Removed unnecessary comments and header from SR_Latch.v

--- SR_Latch.v
+++ SR_Latch.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/12/2025 02:36:50 PM
-// Design Name: 
-// Module Name: SR_Latch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module SR_Latch(
     input R,S,
