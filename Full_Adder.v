29d708c0c590fd039111002a5f49ea2694bf107a
Tanisha227 <tanishameena2115@ce.du.ac.in>
2025-11-12 16:06:14 +0530

    Clean up comments in Full_Adder.v
    Removed unnecessary comments and metadata from Full_Adder.v

--- Full_Adder.v
+++ Full_Adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/12/2025 02:30:14 PM
-// Design Name: 
-// Module Name: Full_Adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Full_Adder(
     input A,B,Cin,
